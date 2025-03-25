48680041f1fb4241f5b9d9264fbf10e3c298f98c
Vikarn Jha <vikarnjha91@gmail.com>
2025-03-25 20:02:44 +0530

    auth routes optimized

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,56 +1,17 @@
-import express from "express";
-import bcrypt from "bcryptjs";
-import jwt from "jsonwebtoken";
-import User from "../models/User.js";
 import dotenv from "dotenv";
+import express from "express";
+import { register } from "../controller/auth.controller.js";
 
 dotenv.config();
 const router = express.Router();
 
 // Register
-router.post("/register", async (req, res) => {
-  try {
-    const { name, email, password, confirmPassword } = req.body;
-    if (password !== confirmPassword) {
-      return res.status(400).json({ message: "Passwords do not match!" });
-    }
-    const hashedPassword = await bcrypt.hash(password, 10);
-    const user = new User({ name, email, password: hashedPassword });
-    await user.save();
-    res.status(201).json({ message: "User registered successfully!" });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: "Server error, try again!" });
-  }
-});
+router.post("/register", register);
 
 // Login
-router.post("/login", async (req, res) => {
-  try {
-    const { email, password } = req.body;
-    const user = await User.findOne({ email });
-    if (!user) {
-      return res.status(400).json({ error: "Invalid email or password" });
-    }
-    const isMatch = await bcrypt.compare(password, user.password);
-    if (!isMatch) {
-      return res.status(400).json({ error: "Invalid email or password" });
-    }
+router.post("/login", login);
 
-    // Generate JWT token
-    const token = jwt.sign(
-      { id: user._id },
-      process.env.JWT_SECRET,
-      { expiresIn: "1h" } // Token expires in 1 hour
-    );
-    res.json({
-      message: "Login successful!",
-      token,
-      user: { id: user._id, name: user.name, email: user.email },
-    });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: "Server error, please try again" });
-  }
-});
+// Logout
+router.get("/logout", logout);
+
 export default router;
