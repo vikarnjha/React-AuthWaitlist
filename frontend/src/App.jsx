7d5a4697cbcb112016643acde7e220c5f70b15c0
Vikarn Jha <vikarnjha91@gmail.com>
2025-03-26 20:27:00 +0530

    Started working on frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import {} from "react";
+import "./App.css";
+import Home from "./component/Home";
+
+
+function App() {
+  return (
+    <>
+      <Home />
+    </>
+  );
+}
+
+export default App;
